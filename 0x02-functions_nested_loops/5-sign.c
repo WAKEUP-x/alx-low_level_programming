8f044d8ae2dd4a0a2645a6be853a0baf3db12f00
WAKEUPx <wakeeupx@gmail.com>
2023-07-18 05:06:37 -0700

    C - Functions, nested loops Done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_sign - determine if the input number
- * greater, equal or less than zero.
- *
- * @n: the input number as an integer.
- *
- * Return: 1 is greater than zero. 0 is zero.
- * -1 is less than zero.
- */
+* print_sign - determine if the input number
+* greater, equal or less than zero.
+*
+* @n: the input number as an integer.
+*
+* Return: 1 is greater than zero. 0 is zero.
+* -1 is less than zero.
+*/
 int print_sign(int n)
 {
         if (n > 0)
